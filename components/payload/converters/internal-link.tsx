3eeef0e8f5e6a485532a86c0670f520cbe742641
Robby Doyle <rmdoyledesign@gmail.com>
2025-06-02 21:48:47 -0700

    Update internal-link.tsx

--- components/payload/converters/internal-link.tsx
+++ components/payload/converters/internal-link.tsx
@@ -3,12 +3,11 @@ import { SerializedLinkNode } from '@payloadcms/richtext-lexical';
 export const internalDocToHref = ({ linkNode }: { linkNode: SerializedLinkNode }) => {
   const { value, relationTo } = linkNode.fields.doc!;
 
-  const slug = typeof value !== 'string' && value.slug;
+  // Check if value is an object and has a slug property
+  const slug = typeof value === 'object' && value !== null && 'slug' in value ? value.slug : '';
 
   if (relationTo === 'posts') {
     return `/blog/${slug}`;
-  } else if (relationTo === 'users') {
-    return `/users/${slug}`;
   } else {
     return `/${slug}`;
   }
